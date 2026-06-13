96717899fefc56672a6ed1413ac3f01340f6b2c3
name <97321565+Jbsousa86@users.noreply.github.com>
2026-06-12 21:02:50 -0300

    android

--- android/app/src/main/java/com/playerremoto/pro/MainActivity.java
+++ android/app/src/main/java/com/playerremoto/pro/MainActivity.java
@@ -1,5 +1,20 @@
 package com.playerremoto.pro;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Remove a trava de segurança que bloqueia a Voz (TTS) e o Gongo no Android (Capacitor)
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
+        }
+    }
+}
